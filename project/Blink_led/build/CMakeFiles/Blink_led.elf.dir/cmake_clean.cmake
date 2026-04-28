81cc323e8db33712b2713ad3226a1d462d9150e7
TruongTien <truongtien05092003@gmail.com>
2026-04-28 14:09:04 +0700

    update blink led

---
+++ project/Blink_led/build/CMakeFiles/Blink_led.elf.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Blink_led.elf"
+  "Blink_led.elf.manifest"
+  "Blink_led.elf.pdb"
+  "CMakeFiles/Blink_led.elf.dir/src/main.c.obj"
+  "CMakeFiles/Blink_led.elf.dir/src/main.c.obj.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Blink_led.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- project/Blink_led/build/CMakeFiles/Blink_led.elf.dir/depend.make
+++ project/Blink_led/build/CMakeFiles/Blink_led.elf.dir/depend.make
@@ -1,2 +1,2 @@
-# Empty dependencies file for main.elf.
+# Empty dependencies file for Blink_led.elf.
 # This may be replaced when dependencies are built.
