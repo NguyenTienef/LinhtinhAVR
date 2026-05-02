38260567a1facefd3727a0cb4f2ef407a029d11d
TruongTien <truongtien05092003@gmail.com>
2026-05-02 11:58:54 +0700

    .\palindrome_number.cpp

--- review/palindrome_number.cpp
+++ review/palindrome_number.cpp
@@ -7,33 +7,34 @@ class palindrome_number
 private:
     /* data */
 public:
-    palindrome_number(/* args */);
-    ~palindrome_number();
+    /* bool isPalindrome(int x) {
+        if(x < 0) {
+            return false;
+        }
+        string s = to_string(x);
+        string rev_s = s;
+        reverse(rev_s.begin(), rev_s.end());
+        return s == rev_s;
+    }*/
+    bool isPalindrome(int x){
+        if (x < 0){
+            return false;
+        }
+        int original = x;
+        long rn = 0;
+        while (x > 0){
+            rn = rn*10 + x%10;
+            x = x/10;
+        }
+        return rn == original;    
+    }
 };
-
-palindrome_number::palindrome_number(/* args */)
-{
-
-
-
-}
-
-palindrome_number::~palindrome_number()
-{
-
-}
 
 int main(){
     int x;
     cin >> x;
-    if(x < 0) {
-        cout << "false";
-        return 0;
-    }
-    string s = to_string(x);
-    string rev_s = s;
-    reverse(rev_s.begin(), rev_s.end());
-    if(s == rev_s) {
+    palindrome_number pn;
+    if(pn.isPalindrome(x)) {
         cout << "true";
     } else {
         cout << "false";
