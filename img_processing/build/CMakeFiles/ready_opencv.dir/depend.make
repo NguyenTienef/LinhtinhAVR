f8b3a0c43c5d58fcbf45bac3ab3a259fe9c5f163
also Truongtien <tiennt.cmhn@gmail.com>
2026-05-31 19:32:18 +0700

    setup success

---
+++ img_processing/build/CMakeFiles/ready_opencv.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ready_opencv.
+# This may be replaced when dependencies are built.
---
+++ img_processing/build/CMakeFiles/simple_test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple_test.dir/src/simple_test.cpp.obj"
+  "CMakeFiles/simple_test.dir/src/simple_test.cpp.obj.d"
+  "libsimple_test.dll.a"
+  "simple_test.exe"
+  "simple_test.exe.manifest"
+  "simple_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simple_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
