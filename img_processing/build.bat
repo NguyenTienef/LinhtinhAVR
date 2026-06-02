4137693d21c25e0c6e8dac59a0f338493f85a4a9
also Truongtien <tiennt.cmhn@gmail.com>
2026-06-02 21:58:05 +0700

    Update path in sys-environtment

--- img_processing/build.bat
+++ img_processing/build.bat
@@ -10,11 +10,11 @@ cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release ..
 echo === Building ===
 cmake --build . -j4
 
-echo === Copying DLLs ===
-cd ..
-copy "C:\Users\Admin\Desktop\Cfile\img_processing\opencv\build\install\x64\mingw\bin\*.dll" ".\build\"
-copy "C:\msys64\ucrt64\bin\libstdc++-6.dll" ".\build\"
-copy "C:\msys64\ucrt64\bin\libgcc_s_seh-1.dll" ".\build\"
-copy "C:\msys64\ucrt64\bin\libwinpthread-1.dll" ".\build\"
+@REM echo === Copying DLLs ===
+@REM cd ..
+@REM copy "C:\Users\Admin\Desktop\Cfile\img_processing\opencv\build\install\x64\mingw\bin\*.dll" ".\build\"
+@REM copy "C:\msys64\ucrt64\bin\libstdc++-6.dll" ".\build\"
+@REM copy "C:\msys64\ucrt64\bin\libgcc_s_seh-1.dll" ".\build\"
+@REM copy "C:\msys64\ucrt64\bin\libwinpthread-1.dll" ".\build\"
 
 echo === Done ===
