f23845406e4563461c1e97a9642395c3a9213d0f
Nguyen Tien <spectrecipher262@gmail.com>
2026-07-05 18:59:48 +0700

    rebuild

---
+++ img_processing/include/config.h
@@ -0,0 +1,8 @@
+// config for normal camera
+#ifndef CONFIG_H
+#define CONFIG_H
+
+
+
+
+#endif // CONFIG_H
--- img_processing/test/Normal_cam.cpp
+++ img_processing/test/Normal_cam.cpp
@@ -1,12 +1,17 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
-
+#include "config.h"
 
 using namespace std;
 
 int main() {
+    
+    // static uint8_t data[100];
+    // uint8_t *ws = data;
+
+    
     // Mở camera mặc định (index 0)
-    cv::VideoCapture cap(0);
+    cv::VideoCapture cap(1);
 
     int value = 255;  // Giá trị độ sáng (0-255)
     cap.set(cv::CAP_PROP_BRIGHTNESS, value);
@@ -24,7 +29,7 @@ int main() {
         // Đọc frame từ camera
         cap >> frame;
 
-        cv::resize(frame, frame, cv::Size(720, 680)); // Thay đổi kích thước nếu cần
+        cv::resize(frame, frame, cv::Size(1366 , 768 )); // Thay đổi kích thước nếu cần
 
         if (frame.empty()) {
             cerr << "Cannot receive frame!" << endl;
@@ -34,10 +39,15 @@ int main() {
         cv::flip(frame, frame, 1); // Lật ảnh ngang (mirror)
 
         // Hiển thị cửa sổ
-        cv::imshow("Camera - Normal", frame);
+        cv::Mat denoised;
+        cv::medianBlur(frame, denoised, 5);  // kernel size = 5
 
-        // Nhấn 'q' để thoát
-        if (cv::waitKey(1) == 'q') {
+        cv::imshow("Camera - Normal", frame);
+        cv::imshow("Denoised", denoised);
+
+        
+        
+        if (cv::waitKey(1) == 'q' || cv::waitKey(1) == 27) { // Nhấn 'q' hoặc 'ESC' để thoát
             cout << "quit program." << endl;
             break;
         }
